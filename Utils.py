ec5228a078555cb1608773b1f7dc102de0331816
Sabrinabd <bouadisabrina02@gmail.com>
2024-12-31 18:26:02 +0100

    Added Extensions class and Dung framework with admissibility and conflict-free set computation

--- Utils.py
+++ Utils.py
@@ -79,3 +79,129 @@ def compute_acceptability(arg, E, relations):
 		else:
 			return False
 
+# Function to check if arguments are defined in relations
+def checkArgumentsInRelations(arguments, relations):
+
+	if len(arguments) > 0:
+		if len(relations) > 0:
+			for x in relations:
+				lst = []
+				status = True
+				if x[0] not in arguments or x[1] not in arguments:
+					status = False
+				lst.append(status)	 
+				if all(lst):
+					return True
+				else:
+					return False
+		else:
+			return True
+	else:
+		return False
+
+# Class for representing extensions
+class Extensions:
+
+	def __init__(self, extensions, arguments):
+		self.__extensions = extensions
+		self.__arguments = arguments
+
+	def get_Extensions(self):
+
+		return self.__extensions
+		
+	def get_SkepticallyAcceptedArguments(self):
+
+		accepted = set()
+		if len(self.__extensions) > 0:
+			for a in self.__arguments:
+				lst = []
+				for extension in self.__extensions:
+					if a in extension:
+						lst.append(True)
+					else:
+						lst.append(False)
+				if all(lst):
+					accepted.add(a)
+		return accepted
+
+	def get_CredulouslyAcceptedArguments(self):
+
+		accepted = set()
+		if len(self.__extensions) > 0:
+			for a in self.__arguments:
+				for extension in self.__extensions:
+					if a in extension:
+						accepted.add(a)
+		return accepted
+
+				
+
+# Class for representing Dung Argumentation Framework
+class Dung:
+
+	def __init__(self, arguments, relations):
+		self.__arguments = arguments
+		self.__relations = relations
+		self.semantics = Dung.Semantics(self)
+
+	def compute_cfs(self):
+
+		args = self.__arguments
+		rel = self.__relations
+
+		pwr = powerset(args)
+
+		la = len(args)
+		lr = len(rel)
+
+		if la > 0:
+			if lr > 0:
+				for x in rel:
+					x1 = x[0]
+					x2 = x[1]
+					dele = []
+					for i in pwr:
+						if (x1 in i) and (x2 in i):
+							dele.append(i)
+					for e in dele:
+						pwr.remove(e)
+		return set(pwr)
+
+
+	def compute_admissibility(self):
+		cfs = self.compute_cfs()
+
+		rel = self.__relations
+
+		admissible = []
+		if checkArgumentsInRelations(self.__arguments, rel) == True:
+			if len(cfs) > 0:
+				for cfset in cfs:
+					attackers = set()
+					for cfsetmember in cfset:
+						attackers = attackers.union(get_arg_attackers(cfsetmember, rel))
+					attackedbycfsmembers = []
+					for attacker in attackers:
+						atk = False
+						attackedby = get_arg_attackers(attacker, rel)
+						for cfsetmember in cfset:
+							if cfsetmember in attackedby:
+								atk = True
+						attackedbycfsmembers.append(atk)
+					if all(attackedbycfsmembers):
+						if cfset == ():
+							admissible.append(set())
+						else:
+							d = set()
+							for k in cfset:
+								for kk in k:
+									d.add(kk)
+							admissible.append(d)
+				return admissible
+			else:
+				return []
+		else:
+			return None
+
+ 
